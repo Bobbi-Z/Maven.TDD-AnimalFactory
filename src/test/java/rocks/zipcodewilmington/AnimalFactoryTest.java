0bcfd677e657fa18e27d5f8e566bb3cca27cce4a
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-19 02:53:47 -0400

    cathouse tests created and passed

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,60 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
+    @Test
+    public void testCreateDog(){
+        DogHouse dogHouse = new DogHouse();
+        dogHouse.add(new Dog("", new Date(), 1));
+
+        String givenName = "Spot";
+        Date givenBD = new Date(2021, 07, 17);
+
+         Dog newDog = AnimalFactory.createDog(givenName, givenBD);
+
+         String actualName = newDog.getName();
+         Date actualDate = newDog.getBirthDate();
+         Integer actualID = newDog.getId();
+
+        Assert.assertEquals(givenName, actualName);
+        Assert.assertEquals(givenBD, actualDate);
+        Assert.assertTrue(1 == actualID);
+    }
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+    @Test
+    public void testCatCreate(){
+        CatHouse catHouse = new CatHouse();
+        catHouse.add(new Cat("", new Date(), 1));
+
+        String givenName = "Patches";
+        Date givenBD = new Date(2021, 07, 17);
+
+        Cat newCat = AnimalFactory.createCat(givenName, givenBD);
+
+        String actualName = newCat.getName();
+        Date actualBD = newCat.getBirthDate();
+        Integer actualID = newCat.getId();
+
+        Assert.assertEquals(givenName, actualName);
+        Assert.assertEquals(givenBD, actualBD);
+        Assert.assertTrue(1 == actualID);
+
+    }
+
+
+
+
 }
--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,12 +1,90 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class CatHouseTest {
     // TODO - Create tests for `void add(Cat cat)`
-    // TODO - Create tests for `void remove(Integer id)`
+    @Test
+    public void testingAddCat(){
+        String expectedName = "fluffy";
+        Date expectedDate = new Date();
+        Integer expectedID = 1200;
+
+        Cat expectedCat = new Cat(expectedName, expectedDate, expectedID);
+        CatHouse catHouse = new CatHouse();
+
+     catHouse.add(expectedCat);
+
+        Assert.assertTrue(1 == catHouse.getNumberOfCats());
+    }
+
     // TODO - Create tests for `void remove(Cat cat)`
+    @Test
+    public void testingRemoveCat(){
+        String expectedName = "fluffy";
+        Date expectedDate = new Date();
+        Integer expectedID = 1200;
+
+        Cat expectedCat = new Cat(expectedName, expectedDate, expectedID);
+        CatHouse catHouse = new CatHouse();
+            catHouse.add(expectedCat);
+            catHouse.remove(expectedCat);
+
+            Assert.assertTrue(0 == catHouse.getNumberOfCats());
+    }
+    // TODO - Create tests for `void remove(Integer id)`
+    @Test
+    public void removeCatByID(){
+        String expectedName = "fluffy";
+        Date expectedDate = new Date();
+        Integer expectedID = 1200;
+
+        Cat expectedCat = new Cat(expectedName, expectedDate, expectedID);
+        CatHouse catHouse = new CatHouse();
+        catHouse.add(expectedCat);
+
+        catHouse.remove(1200);
+
+        Assert.assertTrue(0 == catHouse.getNumberOfCats());
+    }
+
     // TODO - Create tests for `Cat getCatById(Integer id)`
-    // TODO - Create tests for `Integer getNumberOfCats()`
+    @Test
+    public void testGetCatByID(){
+        String expectedName = "fluffy";
+        Date expectedDate = new Date();
+        Integer expectedID = 1200;
+
+        Cat expectedCat = new Cat(expectedName, expectedDate, expectedID);
+        CatHouse catHouse = new CatHouse();
+        catHouse.add(expectedCat);
+
+        Cat actual = catHouse.getCatById(1200);
+
+        Assert.assertEquals(expectedCat, actual);
+    }
+    // TODO - Create tests for `Integer getNumberOfCats()
+    @Test
+    public void testGetNumOfCats(){
+        String expectedName = "fluffy";
+        Date expectedDate = new Date();
+        Integer expectedID = 1200;
+
+        Cat expectedCat = new Cat(expectedName, expectedDate, expectedID);
+        CatHouse catHouse = new CatHouse();
+        catHouse.add(expectedCat);
+
+        Integer expected = 1;
+        Integer actual = catHouse.getNumberOfCats();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
